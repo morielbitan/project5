834ae03b7bff520c1144e540e8a4a70bf7ffb9ce
morielbitan <morielbitan@hilma.tech>
2025-11-11 10:09:22 +0200

    login + regisetr + info for register

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,19 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
-import Info from "./Info";
-import { useState } from "react";
 import Login from "./Login";
+import Register from "./Register";
+import InfoToRegister from "./InfoToRegister";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/info">Info</Link>
-            </li>
-            <li>
-              <Link to="/Albums">Albums</Link>
-            </li>
-            <li>
-              <Link to="/Posts">Posts</Link>
-            </li>
-            <li>
-              <Link to="/Todos">Todos</Link>
-            </li>
-            <li>
-              <Link to="/Logout">Logout</Link>
-            </li>
-          </ul>
-        </nav>
-
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/info" element={<Info />} />
-          {/* <Route path="/Albums" element={<Albums />} /> */}
-          {/* <Route path="/info" element={<Info />} />
-          <Route path="/info" element={<Info />} />
-          <Route path="/info" element={<Info />} />
-          <Route path="/info" element={<Info />} />
-          <Route path="/info" element={<Info />} /> */}
-
-          {/* <Route path="*" element={<Home />} /> */}
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/infoToRegister" element={<InfoToRegister />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,3 +1,56 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Register() {}
+function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit() {
+    if (password === checkPassword) {
+      localStorage.setItem(
+        "userInfo",
+        JSON.stringify({ username: username, website: password })
+      );
+      navigate("/infoToRegister");
+    }
+  }
+
+  return (
+    <>
+      <h1>Register</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              value={checkPassword}
+              onChange={(e) => setCheckPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Register;
